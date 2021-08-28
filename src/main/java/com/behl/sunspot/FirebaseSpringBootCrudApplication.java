347a1bc48c38b83a18dcb124910e09481814eaaf
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-08-28 16:22:08 +0530

    init project commit

---
+++ src/main/java/com/behl/sunspot/FirebaseSpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.behl.sunspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseSpringBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseSpringBootCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/behl/sunspot/FirebaseSpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behl.sunspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseSpringBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
