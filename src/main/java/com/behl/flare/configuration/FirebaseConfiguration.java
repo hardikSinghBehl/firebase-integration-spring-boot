e468a6eb09a3e7eb8eb9e4f39ca0d6b0b49a4e31
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-03-17 17:53:38 +0530

    configuring firebase auth bean

--- src/main/java/com/behl/flare/configuration/FirebaseConfiguration.java
+++ src/main/java/com/behl/flare/configuration/FirebaseConfiguration.java
@@ -10,6 +10,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.cloud.FirestoreClient;
 
 import lombok.RequiredArgsConstructor;
@@ -37,6 +38,11 @@ public class FirebaseConfiguration {
 	@Bean
 	public Firestore firestore(final FirebaseApp firebaseApp) {
 		return FirestoreClient.getFirestore(firebaseApp);
+	}
+	
+	@Bean
+	public FirebaseAuth firebaseAuth(final FirebaseApp firebaseApp) {
+		return FirebaseAuth.getInstance(firebaseApp);
 	}
 
 }
