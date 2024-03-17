297f191c1721580f974d90555e4aabc29210b44f
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-03-17 17:54:01 +0530

    updating firebase configuration properties

--- src/main/java/com/behl/flare/configuration/FirebaseConfigurationProperties.java
+++ src/main/java/com/behl/flare/configuration/FirebaseConfigurationProperties.java
@@ -1,23 +1,32 @@
 package com.behl.flare.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Validated
 @ConfigurationProperties(prefix = "com.behl.flare")
 public class FirebaseConfigurationProperties {
 
+	@Valid
 	private FireBase firebase = new FireBase();
 
 	@Getter
 	@Setter
 	public class FireBase {
 
+		@NotBlank
 		private String privateKey;
-
+		
+		@NotBlank
+		private String webApiKey;
+		
 	}
 
 }
