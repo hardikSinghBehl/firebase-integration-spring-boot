0649fd775cc145e410615b514fe00539646f151e
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-03-18 13:24:23 +0530

    feature/containerize-application (#11)

--- src/main/java/com/behl/flare/FirebaseIntegrationApplication.java
+++ src/main/java/com/behl/flare/FirebaseIntegrationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirebaseSpringBootCrudApplication {
+public class FirebaseIntegrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirebaseSpringBootCrudApplication.class, args);
+		SpringApplication.run(FirebaseIntegrationApplication.class, args);
 	}
 
 }
